68532f13277c4a1533ff82d6cd7e0fce67eb7ae9
Admin Kernel <admin_kernel@kernel.legoff.be>
2021-03-14 00:01:45 +0100

    First commit

--- snap/local/launcher/kresd.sh
+++ snap/local/launcher/kresd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$SNAP/usr/sbin/kresd -c /usr/lib/knot-resolver/distro-preconfig.lua -c /etc/knot-resolver/kresd.conf -n
+$SNAP/usr/sbin/kresd -c $SNAP/usr/lib/knot-resolver/distro-preconfig.lua -c $SNAP/etc/knot-resolver/kresd.conf -n
